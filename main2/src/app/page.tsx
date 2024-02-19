0b72627cffdf5108ba00a1ffad3b97c6bfc727b1
APOSTOLOS KYRGIDIS <apostolos052@gmail.com>
2024-02-19 10:14:08 +0200

    updated

---
+++ main2/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Games from "@/components/Games";
+import Movies from "@/components/Movies";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Games />
+      <Movies />
+    </>
+  );
+}
---
+++ main2/src/components/Games.tsx
@@ -0,0 +1,44 @@
+"use client"; //because we use UseState
+import NextVideo from "next-video";
+import React, { useRef, useState } from "react";
+
+function Games() {
+  const [isPlaying, setisPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  let timer: ReturnType<typeof setTimeout>;
+
+  const handleMouseEnter = () => {
+    timer = setTimeout(() => {
+      if (!isPlaying && videoRef.current) {
+        videoRef.current.play();
+        setisPlaying(true);
+      }
+    }, 400);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(timer);
+    if (isPlaying && videoRef.current) {
+      videoRef.current.pause();
+      setisPlaying(false);
+    }
+  };
+
+  return (
+    <div
+      className="group text-6xl justify-center flex w-2/4 h-screen overflow-hidden grayscale hover:grayscale-0 transition duration-500 ease-in-out cursor-pointer"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h1 className="flex absolute top-2/4 text-white z-10 opacity-0">Games</h1>
+      <NextVideo
+        // src="/games.mp4"
+        controls={false}
+        ref={videoRef}
+        muted
+      />
+    </div>
+  );
+}
+
+export default Games;

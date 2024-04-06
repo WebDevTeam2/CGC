b66209c7a31b40f02c816ff65e1f58b7830198c8
Stefanos2002 <kalouliss@gmail.com>
2024-04-06 17:14:16 +0300

    .

--- app/Games/[name]/page.tsx
+++ app/Games/[name]/page.tsx
@@ -25,6 +25,14 @@ interface PostPage {
       };
     }
   ];
+  platforms: [
+    {
+      platform: {
+        name: string;
+        slug: string;
+      };
+    }
+  ];
   released: string;
   tba: boolean;
   background_image: string;
@@ -53,7 +61,36 @@ export default async function Games({ params }: { params: PostPage }) {
   return (
     <div>
       <div className="bg-slate-700 fixed h-screen w-full"></div>
-      <div className="flex pt-10 flex-col items-center justify-center">
+      <div className="flex flex-row pt-10 justify-evenly">
+        <div className="flex flex-col relative pt-24">
+          <Image
+            src={game.background_image}
+            alt={game.name}
+            width={600}
+            height={600}
+          />
+          <div className="relative flex flex-col -top-10">
+            <div className="fade-bottom"></div>
+            <div className="flex flex-col gap-2 text-lg items-center pt-6 border-0 text-center font-inter text-white bg-black rounded-b-xl h-[22vw]">
+              <span>
+                Rating: {game.rating} / {game.rating_top}
+              </span>
+              <span>Release date: {game.released}</span>
+              <span className="w-[35rem]">
+                Platforms:{" "}
+                {game.platforms.map(
+                  (platform: { platform: { name: string } }, index: number) => (
+                    <span key={index} className="">
+                      {index > 0 && ","}{" "}
+                      {/* Add slash if not the first platform */}
+                      {platform.platform.name}
+                    </span>
+                  )
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
         <span className="font-inter leading-8 border relative 2xl:w-1/2 xl:w-4/6 w-5/6 bg-stone-900/60 p-6 rounded-2xl text-balance text-white text-xl transition-[width] ease-in-out duration-300">
           {stripHtmlTags(game.description)}
         </span>

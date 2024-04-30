1134f1511ab172dc3ee2aa9d26f8a745664703e0
UnLakhs <apostolos052@gmail.com>
2024-04-30 20:39:37 +0300

    Finished Random

--- app/components/Movie-components/Nav-items/Random.tsx
+++ app/components/Movie-components/Nav-items/Random.tsx
@@ -2,63 +2,41 @@ import Link from "next/link";
 import { TiArrowShuffle } from "react-icons/ti";
 import { useState, useEffect } from "react";
 
-//API DATA
-const apiKey = "api_key=a48ad289c60fd0bb3fc9cc3663937d7b";
-const baseUrl = "https://api.themoviedb.org/3/movie/";
-const TODAY = new Date().toISOString().split("T")[0];
-
 interface MovieDetails {
-  adult: boolean;
-  backdrop_path: string;
-  genres: Genre[];
   id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-interface Genre {
-  id: number;
-  name: string;
 }
 
 const Random = () => {
-  const [movieData, setMovieData] = useState<number[]>([]);
-  const [randomId, setRandomId] = useState<number | null>(2);
+  const [movieData, setMovieData] = useState<MovieDetails[]>([]);   //Vazw ola ta ids apo tis tainies tou json se ena array
+  const [randomId, setRandomId] = useState<number>(2);       
 
   useEffect(() => {
-    fetch('../../Movies/data/movie_ids_04_24_2024.json')
-      .then(res => res.text())
+    fetch("/data/movie_ids_04_24_2024.json")
+      .then((res) => res.text())
       .then((data) => {
         const lines = data.trim().split("\n");
         const movies: MovieDetails[] = lines.map((line: string) =>
           JSON.parse(line)
         );
-        setMovieData(movies.map((movie) => movie.id));
+        setMovieData(movies);
       })
-
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
   const getRandomId = () => {
-    const randomIndex = Math.floor(Math.random() * movieData.length);
-    const randomMovie =  movieData[randomIndex];
-    setRandomId(randomMovie);
+    if (movieData.length > 0) {
+      const randomIndex = Math.floor(Math.random() * movieData.length);
+      const randomMovie = movieData[randomIndex];
+      setRandomId(randomMovie.id);
+    }
   };
-  
+
   return (
     <div>
       <Link
-      onClick={getRandomId}
+        onClick={getRandomId}
         className="flex flex-row items-center h-20 gap-2 not-search"
         href={`/Movies/${randomId}`}
       >

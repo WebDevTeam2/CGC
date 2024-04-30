95666267911e3f53eb17a8c3895b6f2265db3f45
UnLakhs <apostolos052@gmail.com>
2024-04-30 22:16:49 +0300

    page number progress

--- app/components/Movie-components/Pages.tsx
+++ app/components/Movie-components/Pages.tsx
@@ -1,14 +1,39 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const Pages = () => {
+  const [page, setPage] = useState(1);
+
+  const nextPage = () => {
+    setPage((prevPage) => {
+      if (prevPage < pages.length) {
+        return prevPage + 1;
+      } else {
+        return prevPage;
+      }
+    });
+  };
+
+  const previousPage = () => {
+    setPage((prevPage) => {
+      if (prevPage > 1) {
+        return prevPage - 1;
+      } else {
+        return prevPage;
+      }
+    });
+  };
   return (
-    <div className="flex flex-row ml-20">
-        {pages.map((page) => (
-            <Link key={page} href={`/Movies/moviePage/${page}`}>{page}</Link>
-        ))}
-      
+    <div className="flex flex-row ml-[25rem] gap-2 text-lg">
+      {}
+      {pages.map((pageNumber) => (
+        <div key={pageNumber}>
+          <Link href={`/Movies/moviePage/${pageNumber}`}>{pageNumber}</Link>
+        </div>
+      ))}
     </div>
   );
 };

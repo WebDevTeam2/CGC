9262c19d4d0321275583f4881425d130812922a3
UnLakhs <apostolos052@gmail.com>
2024-05-01 16:04:43 +0300

    pages atempt

--- app/components/Movie-components/Pages.tsx
+++ app/components/Movie-components/Pages.tsx
@@ -1,37 +1,47 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+// ΝΟΤΕ: TO PAGE DEN KANEI UPDATE SWSTA
+// TO SETPAGE KANEI UPDATE THN SELIDA KAI META APO LIGO EPANERXETAI STO 1
+// STEFANE VOHTHEIA THA TO SPASW
 
 const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const Pages = () => {
   const [page, setPage] = useState(1);
 
+  const goToPage = (pageNumber: number) => {
+    setPage(pageNumber);
+  };
+
   const nextPage = () => {
-    setPage((prevPage) => {
-      if (prevPage < pages.length) {
-        return prevPage + 1;
-      } else {
-        return prevPage;
-      }
-    });
+    if (page < pages.length) {
+      setPage(page + 1);
+    }
   };
 
   const previousPage = () => {
-    setPage((prevPage) => {
-      if (prevPage > 1) {
-        return prevPage - 1;
-      } else {
-        return prevPage;
-      }
-    });
+    if (page > 1) {
+      setPage(page - 1);
+      
+    }
   };
+
+  useEffect(() => {
+    console.log(page);
+  }, [page]);
+
   return (
     <div className="flex flex-row ml-[25rem] gap-2 text-lg">
-      {}
       {pages.map((pageNumber) => (
         <div key={pageNumber}>
-          <Link href={`/Movies/moviePage/${pageNumber}`}>{pageNumber}</Link>
+          <Link
+            onClick={() => goToPage(pageNumber)}
+            href={`/Movies/moviePage/${pageNumber}`}
+          >
+            {pageNumber}
+          </Link>
         </div>
       ))}
     </div>

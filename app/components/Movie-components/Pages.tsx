f4f002d10a7ecf268f68e29aea086e6e331d287f
UnLakhs <apostolos052@gmail.com>
2024-05-09 21:15:43 +0300

    fixed page counter

--- app/components/Movie-components/Pages.tsx
+++ app/components/Movie-components/Pages.tsx
@@ -2,50 +2,50 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
-// ΝΟΤΕ: TO PAGE DEN KANEI UPDATE SWSTA
-// TO SETPAGE KANEI UPDATE THN SELIDA KAI META APO LIGO EPANERXETAI STO 1
-// STEFANE VOHTHEIA THA TO SPASW
-
 const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const Pages = () => {
   const [page, setPage] = useState(1);
 
-  const goToPage = (pageNumber: number) => {
-    setPage(pageNumber);
-  };
-
-  const nextPage = () => {
-    if (page < pages.length) {
-      setPage(page + 1);
-    }
-  };
-
-  const previousPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
-      
-    }
-  };
-
+  //Xrhsimopoioume ayto to useEffect gia na paroume to page number mesa apo to URL
   useEffect(() => {
-    console.log(page);
-  }, [page]);
-
+    const pathname = window.location.pathname;
+    if (pathname) {
+      const currentPage = parseInt(pathname.split("/").pop() || "");  //Gia na paroume to page number apo to url
+      //An to currentPage yparxei tote kanoume set to page
+      if (!isNaN(currentPage) && currentPage !== page) {
+        setPage(currentPage);
+      }
+    }
+  }, []);
   return (
     <div className="flex flex-row mb-2 ml-[40rem] gap-4 text-lg">
-      
+      <Link
+        className="hover:opacity-[0.5] transition duration-200"
+        href={`/Movies/moviePage/${page - 1}`}
+      >
+        {"<"}
+      </Link>
       {pages.map((pageNumber) => (
         <div key={pageNumber}>
           <Link
-            onClick={() => goToPage(pageNumber)}
             href={`/Movies/moviePage/${pageNumber}`}
-            className={`${pageNumber === page ? 'current-page p-1 text-white rounded-full bg-[#4c545b]' : 'other'} hover:opacity-[0.5] transition duration-200`} //Kanoume target me styles to page pou vrisketai o xrhsrhs
+            className={`${
+              pageNumber === page
+                ? "current-page p-1 text-white rounded-full bg-[#4c545b]"
+                : "other"
+            } hover:opacity-[0.5] transition duration-200`} //Kanoume target me styles to page pou vrisketai o xrhsrhs
           >
             {pageNumber}
           </Link>
         </div>
       ))}
+      <Link
+        className="hover:opacity-[0.5] transition duration-200"
+        href={`/Movies/moviePage/${page + 1}`}
+      >
+        {">"}
+      </Link>
     </div>
   );
 };

f172d6566d644b51d4d34959ab3b41f1262534e5
Stefanos2002 <kalouliss@gmail.com>
2024-05-14 22:38:39 +0300

    all game links are now loading

--- app/components/Game-components/Sort.tsx
+++ app/components/Game-components/Sort.tsx
@@ -14,7 +14,7 @@ const Sort = () => {
         className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700"
         type="button"
       >
-        Dropdown hover
+        Sort By:
         <svg
           className="w-2.5 h-2.5 ms-3"
           aria-hidden="true"

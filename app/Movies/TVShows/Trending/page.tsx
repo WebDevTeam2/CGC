8905101aa57709bd821dd059ea6985cbeb56e9ec
UnLakhs <apostolos052@gmail.com>
2024-06-02 02:20:21 +0300

    minor bug fixes

--- app/Movies/TVShows/Trending/page.tsx
+++ app/Movies/TVShows/Trending/page.tsx
@@ -7,6 +7,14 @@ const baseUrl = "https://api.themoviedb.org/3/";
 const ApiURL = baseUrl + "trending/tv/day?page=1&language=en-US&" + apiKey;
 const imageURL = "https://image.tmdb.org/t/p/w500";
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDhhZDI4OWM2MGZkMGJiM2ZjOWNjMzY2MzkzN2Q3YiIsInN1YiI6IjY1ZTAzYzE3Zjg1OTU4MDE4NjRlZDFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K9v9OEoLELW62sfz4qnwX7lhqTrmT6AipOjL0UlI5vY'
+  }
+};
+
 interface TVShow {
   page: number;
   results: TVShowResult[];
@@ -22,7 +30,7 @@ interface TVShowResult {
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: string;
+  first_air_date: string;
   name: string;
   video: boolean;
   vote_average: number;
@@ -30,8 +38,9 @@ interface TVShowResult {
 }
 
 const getTVShowData = async (url: string) => {
-  const res = await fetch(url);
+  const res = await fetch(url, options);
   const data = await res.json();
+  console.log(url);
   return data;
 };
 
@@ -46,7 +55,8 @@ const getVotecolor = (vote: number) => {
 };
 
 const Trending = async () => {
-  const movieData: TVShow = await getTVShowData(ApiURL);
+  const tvShowData: TVShow = await getTVShowData(ApiURL);
+  const currentDate = new Date().toISOString().split('T')[0];
 
   return (
     <div className="overflow-hidden">
@@ -60,43 +70,44 @@ const Trending = async () => {
         </Link>
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-3/4 sm:ml-20 md:ml-32 lg:ml-64 h-full mt-4 not-search">
-        {/* Kanw Link oloklhrh th kartela */}
-        {movieData.results.map((item) => (
-          <Link
-            key={item.id}
-            href={`/Movies/${item.id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="lg:hover:scale-110 w-full transition duration-700 ease-in-out mb-6 "
-          >
-            {/* image dipla apo ta images me ta noumera */}
-            <div className="sm:w-full sm:h-56 lg:w-full lg:h-96 p-10 relative">
-              <Image
-                src={`${imageURL}${item.poster_path}`}
-                alt={item.name}
-                layout="fill"
-                objectFit="cover"
-                className="w-full h-full absolute"
-                priority
-              />
-            </div>
-            <div className="bg-[#4c545b] h-44 gap-4 cards">
-              <div className="flex ml-4 text-white">
-                <h2 className="">{item.name}</h2>
-                <span
-                  className={`${getVotecolor(
-                    item.vote_average
-                  )} ml-auto mr-5 mt-11`}
-                >
-                  {item.vote_average.toString().slice(0, 3)}
-                </span>
-              </div>
-              <p className="mt-4 ml-4 text-white">
-                {item.overview.slice(0, 40)}...
-              </p>
-            </div>
-          </Link>
-        ))}
+        {tvShowData.results.filter(item => item.first_air_date  <= currentDate).map(
+          (item) =>
+            item.overview && (
+              <Link
+                key={item.id}
+                href={`/Movies/TVShows/${item.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="lg:hover:scale-110 w-full transition duration-700 ease-in-out mb-6 "
+              >
+                <div className="sm:w-full sm:h-56 lg:w-full lg:h-96 p-10 relative">
+                  <Image
+                    src={`${imageURL}${item.poster_path}`}
+                    alt={item.name}
+                    layout="fill"
+                    objectFit="cover"
+                    className="w-full h-full absolute"
+                    priority
+                  />
+                </div>
+                <div className="bg-[#4c545b] h-44 gap-4 cards">
+                  <div className="flex ml-4 text-white">
+                    <h2 className="">{item.name}</h2>
+                    <span
+                      className={`${getVotecolor(
+                        item.vote_average
+                      )} ml-auto mr-5 mt-11`}
+                    >
+                      {item.vote_average.toString().slice(0, 3)}
+                    </span>
+                  </div>
+                  <p className="mt-4 ml-4 text-white">
+                    {item.overview.slice(0, 40)}...
+                  </p>
+                </div>
+              </Link>
+            )
+        )}
       </div>
     </div>
   );
--- app/Movies/moviePage/[page]/page.tsx
+++ app/Movies/moviePage/[page]/page.tsx
@@ -5,6 +5,15 @@ const apiKey = "api_key=a48ad289c60fd0bb3fc9cc3663937d7b";
 const baseUrl = "https://api.themoviedb.org/3/";
 const imageURL = "https://image.tmdb.org/t/p/w500";
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDhhZDI4OWM2MGZkMGJiM2ZjOWNjMzY2MzkzN2Q3YiIsInN1YiI6IjY1ZTAzYzE3Zjg1OTU4MDE4NjRlZDFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K9v9OEoLELW62sfz4qnwX7lhqTrmT6AipOjL0UlI5vY",
+  },
+};
+
 interface Movie {
   page: number;
   results: MovieResult[];
@@ -28,11 +37,13 @@ interface MovieResult {
 }
 
 const getMovieData = async (page: string) => {
-  const res = await fetch(`${baseUrl}discover/movie?include_adult=false&page=${page}&${apiKey}`);
+  const res = await fetch(
+    `${baseUrl}discover/movie?include_adult=false&page=${page}&${apiKey}`,
+    options
+  );
   const data = await res.json();
   return data;
 };
-
 
 const getVotecolor = (vote: number) => {
   if (vote >= 8) {
@@ -46,47 +57,50 @@ const getVotecolor = (vote: number) => {
 
 const Page = async ({ params }: { params: Movie }) => {
   const movieData: Movie = await getMovieData(`${params.page.toString()}`);
+  const currentDate = new Date().toISOString().split("T")[0];
 
   return (
     <div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-3/4 sm:ml-20 md:ml-32 lg:ml-64 mt-4 h-full not-search">
         {/* Kanw Link oloklhrh th kartela */}
-        {movieData.results.map((item) => (
-          <Link
-            key={item.id}
-            href={`/Movies/${item.id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="lg:hover:scale-110 w-full transition duration-700 ease-in-out mb-6 card-link"
-          >
-            {/* image dipla apo ta images me ta noumera */}
-            <div className="sm:w-full sm:h-56 lg:w-full lg:h-96 p-10 relative image-div">
-              <Image
-                src={`${imageURL}${item.poster_path}`}
-                alt={item.title}
-                layout="fill"
-                objectFit="cover"
-                className="w-full h-full absolute"
-                priority
-              />
-            </div>
-            <div className="bg-[#4c545b] h-44 gap-4 cards">
-              <div className="flex ml-4 text-white">
-                <h2 className="">{item.title}</h2>
-                <span
-                  className={`${getVotecolor(
-                    item.vote_average
-                  )} ml-auto mr-5 mt-11`}
-                >
-                  {item.vote_average.toString().slice(0, 3)}
-                </span>
+        {movieData.results
+          .filter((item) => item.release_date <= currentDate)
+          .map((item) => (
+            <Link
+              key={item.id}
+              href={`/Movies/${item.id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="lg:hover:scale-110 w-full transition duration-700 ease-in-out mb-6 card-link"
+            >
+              {/* image dipla apo ta images me ta noumera */}
+              <div className="sm:w-full sm:h-56 lg:w-full lg:h-96 p-10 relative image-div">
+                <Image
+                  src={`${imageURL}${item.poster_path}`}
+                  alt={item.title}
+                  layout="fill"
+                  objectFit="cover"
+                  className="w-full h-full absolute"
+                  priority
+                />
               </div>
-              <p className="mt-4 ml-4 text-white">
-                {item.overview.slice(0, 40)}...
-              </p>
-            </div>
-          </Link>
-        ))}
+              <div className="bg-[#4c545b] h-44 gap-4 cards">
+                <div className="flex ml-4 text-white">
+                  <h2 className="">{item.title}</h2>
+                  <span
+                    className={`${getVotecolor(
+                      item.vote_average
+                    )} ml-auto mr-5 mt-11`}
+                  >
+                    {item.vote_average.toString().slice(0, 3)}
+                  </span>
+                </div>
+                <p className="mt-4 ml-4 text-white">
+                  {item.overview.slice(0, 40)}...
+                </p>
+              </div>
+            </Link>
+          ))}
       </div>
       <div className="">
         <Pages />

901057d5f5d33cbb187d1f858adabf3caca04404
Stefanos2002 <kalouliss@gmail.com>
2024-09-08 21:18:19 +0300

    .

--- app/account/info/page.tsx
+++ app/account/info/page.tsx
@@ -11,25 +11,25 @@ const Account = () => {
 
   return (
     <div className="bg-[#eee3cb] h-screen flex text-center justify-center">
-      <div className="flex justify-center items-center shadow-lg my-12 bg-[#47475c]">
+      <div className="flex justify-center rounded-2xl items-center shadow-lg my-12 bg-[#47475c]">
         <UserOptions />
         {/* option content */}
-        <div className="flex flex-col items-center gap-8 h-full lg:mt-12">
-          <div className="relative w-20 h-20 lg:ml-6 block rounded-full overflow-hidden">
+        <div className="flex flex-col mr-10 items-center gap-6 h-full lg:mt-12">
+          <div className="relative w-20 h-20 rounded-full overflow-hidden">
             <Image
-              src={session?.user?.image || "/assets/images/batman.jpg"} // An o xrhsths exei diko tou image to kanoume display alliws kanoume display ena default
+              src={session?.user?.image || "/assets/images/default_avatar.jpg"} // An o xrhsths exei diko tou image to kanoume display alliws kanoume display ena default
               alt="User Avatar"
-              layout="fill"
+              fill={true}
               className="object-cover"
             />
           </div>
-          <div className="flex flex-row gap-6 text-[#cccccc]">  
-            <span>Username: </span>
-            <span>{session?.user?.name}</span>
+          <div className="flex flex-row gap-2">
+            <span className="text-[#cccccc]">Username: </span>
+            <span className="text-white">{session?.user?.name}</span>
           </div>
-          <div className="flex flex-row gap-6 text-[#cccccc]">
-            <span>email: </span>
-            <span>{session?.user?.email}</span>
+          <div className="flex flex-row gap-2">
+            <span className="text-[#cccccc]">Email: </span>
+            <span className="text-white">{session?.user?.email}</span>
           </div>
         </div>
       </div>
--- app/components/Account-components/UserOptions.tsx
+++ app/components/Account-components/UserOptions.tsx
@@ -4,18 +4,23 @@ import { useState, useEffect } from "react";
 import { signOut } from "next-auth/react";
 
 const UserOptions = () => {
-    const { data: session } = useSession();
-    
-    return (
-        <div className="bg-[#23232e] mr-6 h-full">
-        <ul className="text-[#b6b6b6] flex lg:mt-12 flex-col gap-8">
-          <li className="mx-[1.5rem] cursor-pointer">Account Details</li>
-          <li className="mx-[1.5rem] cursor-pointer">Games</li>
-          <li className="mx-[1.5rem] cursor-pointer">Movies</li>
-          <li className="mx-[1.5rem] cursor-pointer" onClick={() => signOut({ callbackUrl: "/" })}>Sign out</li>
-        </ul>
-      </div>
-    );
-}
+  const { data: session } = useSession();
+
+  return (
+    <div className="bg-[#23232e] mr-8 rounded-l-2xl h-full">
+      <ul className="text-[#b6b6b6] divide-y flex flex-col gap-0">
+        <li className="cursor-pointer p-4">Account Details</li>
+        <li className="cursor-pointer p-4">Games</li>
+        <li className="cursor-pointer p-4">Movies</li>
+        <li
+          className="cursor-pointer p-4"
+          onClick={() => signOut({ callbackUrl: "/" })}
+        >
+          Sign out
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default UserOptions;

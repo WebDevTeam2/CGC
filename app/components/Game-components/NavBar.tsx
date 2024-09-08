f07b1e082181363f4dbe91df474bba8bd171737a
Stefanos2002 <kalouliss@gmail.com>
2024-09-08 20:44:10 +0300

    made some changes to the navbar

--- app/components/Game-components/NavBar.tsx
+++ app/components/Game-components/NavBar.tsx
@@ -68,7 +68,7 @@ const NavBar = ({ parent_platforms }: { parent_platforms: Platform[] }) => {
   };
 
   return (
-    <nav className="w-full flex justify-between items-center sm:pl-6 pl-4  sticky top-0 bg-black h-[10vh] z-20">
+    <nav className="w-full flex justify-between items-center sm:pl-6 pl-4  sticky top-0 bg-black h-20 z-20">
       <div className="left-side-elements overflow-hidden h-full flex-1 pointer-events-none">
         <Link href="/" className="flex items-center h-full">
           <button className="bg-stone-300 pointer-events-auto text-4xl text-stone-800 transition delay-50 p-1 rounded-full hover:brightness-50">
@@ -116,7 +116,7 @@ const NavBar = ({ parent_platforms }: { parent_platforms: Platform[] }) => {
         </div>
       )}
 
-      <div className="right-side-elements h-full">
+      <div className="right-side-elements flex items-center w-28 h-full">
         <div
           ref={menuRef}
           style={{ transitionProperty: "transform" }}
@@ -124,7 +124,7 @@ const NavBar = ({ parent_platforms }: { parent_platforms: Platform[] }) => {
             showmenu
               ? "max-[640px]:translate-x-0"
               : "max-[640px]:translate-x-28"
-          } w-36 transition-all duration-300 ease-in-out flex sm:flex-col flex-row items-center mt-3 max-[640px]:absolute max-[640px]:right-0 max-[640px]:top-44`}
+          } w-36 transition-all duration-300 ease-in-out flex sm:flex-col flex-row items-center mt-0 max-[640px]:absolute max-[640px]:right-0 max-[640px]:top-44`}
         >
           <button
             className="text-white sm:rounded-full sm:p-2 sm:hover:bg-neutral-800 transition delay-75 ease-in-out sm:text-5xl text-xl max-[640px]:bg-neutral-700/50 hover:bg-neutral-900/70 rounded-tl-full rounded-bl-full py-6 px-2"
@@ -140,7 +140,7 @@ const NavBar = ({ parent_platforms }: { parent_platforms: Platform[] }) => {
               showmenu
                 ? "sm:max-h-[30rem] sm:opacity-100"
                 : "sm:max-h-0 sm:opacity-0"
-            }  bg-black transition-all duration-300 ease-in-out overflow-hidden rounded-lg sm:p-4 p-2 gap-5 flex items-center justify-center flex-col`}
+            }  bg-black py-3 absolute z-30 right-0 top-16 transition-all duration-300 ease-in-out overflow-hidden rounded-b-lg  gap-5 flex items-center justify-center flex-col`}
           >
             {logos.map((logo) => (
               <Link key={logo.key} href={`/Games/${logo.slug}/page/1`}>
@@ -155,21 +155,21 @@ const NavBar = ({ parent_platforms }: { parent_platforms: Platform[] }) => {
             {!session ? (
               <>
                 <Link href={"/Signup"}>
-                  <button className="text-stone-200 sm:text-xl text-lg uppercase transition delay-50 p-2 rounded-full hover:scale-110">
+                  <button className="text-stone-200 sm:text-xl text-lg transition delay-50 p-2 rounded-full hover:scale-110">
                     Sign Up
                   </button>
                 </Link>
                 <Link href={"/Signin"}>
-                  <button className="text-stone-200 sm:text-xl text-lg uppercase transition delay-50 p-2 rounded-full hover:scale-110">
+                  <button className="text-stone-200 sm:text-xl text-lg transition delay-50 p-2 rounded-full hover:scale-110">
                     Log In
                   </button>
                 </Link>
               </>
             ) : (
-              <Logout />
+              <div className="hidden"></div>
             )}
             <Link href={"/Games/page/1"}>
-              <button className="text-stone-200 uppercase sm:text-xl text-lg transition delay-50 p-2 rounded-full hover:scale-110">
+              <button className="text-stone-200 sm:text-xl text-lg transition delay-50 p-2 rounded-full hover:scale-110">
                 All Games
               </button>
             </Link>

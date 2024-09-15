dc18d52c98b9e9ff4cb61b61b0467e757e861e89
UnLakhs <apostolos052@gmail.com>
2024-09-15 05:38:30 +0300

    fixed update in user info

--- app/api/users/[userid]/route.ts
+++ app/api/users/[userid]/route.ts
@@ -8,11 +8,26 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { userid: string } }
 ) {
-  const  userid  = params.userid;
+  const userid = params.userid;
   try {
     const result = await User.findOne({ _id: userid });
-    return NextResponse.json({success: true, data: result});
-  } catch(err) {
-    return NextResponse.json({success: false, message: "No data found"});
+    return NextResponse.json({ success: true, data: result });
+  } catch (err) {
+    return NextResponse.json({ success: false, message: "No data found" });
   }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { userid: string } }
+) {
+  const userid = params.userid;
+  const { username, email } = await req.json();
+  try {
+    const result = await User.findOneAndUpdate({ _id: userid }, { username, email });
+    return NextResponse.json({ success: true, data: result });
+  }
+  catch (err) {
+    return NextResponse.json({ success: false, message: "No data found" });
+  } 
 }

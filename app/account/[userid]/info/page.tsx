8bc9b9dbbcd981806030b99ecd68303f3a1258ea
Stefanos2002 <kalouliss@gmail.com>
2024-09-16 12:13:13 +0300

    .

--- app/account/[userid]/info/page.tsx
+++ app/account/[userid]/info/page.tsx
@@ -5,6 +5,7 @@ import UserOptions from "@/app/components/Account-components/UserOptions";
 //utills
 import { useEffect, useState } from "react";
 import Image from "next/legacy/image";
+import Link from "next/link";
 
 const Account = ({ params }: { params: { userid: string } }) => {
   const [user, setUser] = useState<any>(null);
@@ -65,10 +66,15 @@ const Account = ({ params }: { params: { userid: string } }) => {
 
   return (
     <div className="back-img h-screen flex text-center justify-center">
+      <Link href={`/`} className="absolute pointer-events-none">
+        <h2 className="ml-4 mt-4 text-white pointer-events-auto text-2xl transition duration-100 p-1 rounded-full hover:scale-110">
+          &#8618; Home
+        </h2>
+      </Link>
       {isSuccess && user ? (
-        <div className="flex justify-center rounded-2xl items-center shadow-lg my-28 bg-slate-300">
+        <div className="flex w-1/2 rounded-2xl items-center shadow-lg my-28 bg-slate-300">
           <UserOptions />
-          <div className="flex flex-col h-full items-center mr-10 gap-0 mt-12">
+          <div className="flex flex-col flex-grow h-full items-center mr-10 gap-0 mt-12">
             <div className="relative w-20 h-20 rounded-full overflow-hidden">
               <Image
                 src={user.profilePicture || "/assets/images/default_avatar.jpg"}
--- app/components/Account-components/UserOptions.tsx
+++ app/components/Account-components/UserOptions.tsx
@@ -6,21 +6,55 @@ import Link from "next/link";
 
 const UserOptions = () => {
   const { data: session } = useSession();
+  const [userId, setUserId] = useState<string>("");
+
+  // // Fetch the user's details from the database on component mount
+  useEffect(() => {
+    const fetchProfileDetails = async () => {
+      if (session?.user?.email) {
+        try {
+          // Fetch response using the email as a query param
+          const response = await fetch(
+            `/api/getUserDetails?email=${session.user.email}`
+          );
+
+          if (!response.ok) {
+            console.error("Error fetching user details:", response.statusText);
+            return;
+          }
+
+          // Parse the response as JSON
+          const data = await response.json();
+
+          // Check if the data contains a valid id
+          if (data?._id) {
+            setUserId(data._id);
+          } else {
+            console.log("No profile found for this user.");
+          }
+        } catch (error) {
+          console.error("Failed to fetch profile details:", error);
+        }
+      }
+    };
+
+    fetchProfileDetails();
+  }, [session?.user?.email]); // Only re-run this effect if the session changes\
 
   return (
     <div className="bg-[#23232e] mr-8 rounded-l-2xl h-full">
       <ul className="text-[#b6b6b6] pt-2 divide-y flex flex-col">
-        <Link href={"/account/info"}>
+        <Link href={`/account/${userId}/info`}>
           <li className="cursor-pointer transition-all duration-200 hover:text-white p-4">
             Account Details
           </li>
         </Link>
-        <Link href={"/account/games"}>
+        <Link href={`/account/${userId}/games`}>
           <li className="cursor-pointer transition-all duration-200 hover:text-white p-4">
             Games
           </li>
         </Link>
-        <Link href={"/account/movies"}>
+        <Link href={`/account/${userId}/movies`}>
           <li className="cursor-pointer transition-all duration-200 hover:text-white p-4">
             Movies
           </li>

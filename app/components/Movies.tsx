1883684063dd9ab770f7a0185f36521978cfef02
Stefanos2002 <kalouliss@gmail.com>
2024-10-23 19:55:07 +0300

    .

--- app/components/Movies.tsx
+++ app/components/Movies.tsx
@@ -57,18 +57,10 @@ const Movies = () => {
       // onMouseEnter={handleMouseEnter}
       // onMouseLeave={handleMouseLeave}
     >
-      <Image
-        src={jokerImagePath}
-        alt="Joker image placeholder"
-        layout="fill"
-        objectFit="cover"
-        className="absolute grayscale hover:grayscale-0 transition duration-1000  "
-        priority
-      ></Image>
       <img
         src={jokerImagePath}
         alt="Joker image placeholder"
-        className="absolute object-cover grayscale hover:grayscale-0 transition duration-1000  "
+        className="absolute object-cover h-full w-full grayscale hover:grayscale-0 transition duration-1000  "
       />
       <h1 className="clip-text group-hover:opacity-100 transition duration-1000 flex absolute top-2/4 text-white z-10 lg:text-[4rem] text-[2rem] lg:opacity-0">
         Movies

df971bbd937f1979757eb4226b53acc98363346a
UnLakhs <apostolos052@gmail.com>
2024-10-24 21:00:10 +0300

    fixed img in nav

--- app/Movies/moviePage/[page]/page.tsx
+++ app/Movies/moviePage/[page]/page.tsx
@@ -54,7 +54,7 @@ const Page = async ({ params }: { params: Movie }) => {
   return (
     <div className="overflow-hidden">
       <Filter />
-      <div className="grid msm:grid-cols-2 msm:gap-y-2 msm:mx-auto msm:w-[29rem] md:grid-cols-3 lg:grid-cols-4 md:gap-8 lg:gap-8 lg:w-3/4 md:w-[80%] md:ml-32 lg:ml-64 mt-4 h-full not-search movies-grid">
+      <div className="grid msm:grid-cols-2 msm:gap-y-2 msm:mx-auto msm:w-[92%] md:grid-cols-3 lg:grid-cols-4 md:gap-8 lg:gap-8 lg:w-3/4 md:w-[80%] md:ml-32 lg:ml-64 mt-4 h-full not-search movies-grid">
         {/* We display the results based on their release year */}
         {movieData.results
           .filter((item) => item.release_date <= currentDate)

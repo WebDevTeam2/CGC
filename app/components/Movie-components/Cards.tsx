526bf40155473a4d17fd2707eebc90c75fd54dcc
UnLakhs <apostolos052@gmail.com>
2024-10-26 01:20:36 +0300

    added new branch

---
+++ app/components/Movie-components/Cards.tsx
@@ -0,0 +1,99 @@
+import {
+  getVotecolor,
+  imageURL,
+  Movie,
+  MovieProps,
+} from "@/app/constants/constants";
+import { FaStar } from "react-icons/fa";
+import Link from "next/link";
+import AddToWatchlist from "./AddToWatchlist";
+
+const Cards = ({ movieData, upcoming, movieResultData }: MovieProps ) => {
+  const currentDate = new Date().toISOString().split("T")[0];
+  return upcoming && movieResultData ? (
+    <>
+      {movieResultData.map((item) => (
+        <Link
+          key={item.id}
+          href={`/Movies/${item.id}`}
+          className="lg:hover:scale-110 lg:w-full md:w-[90%] transition duration-700 ease-in-out mb-6 card-link"
+        >
+          {/* Container for the image and content */}
+          <div className="flex flex-col items-center">
+            {/* Image container */}
+            <div className="relative w-full h-56 sm:h-56 lg:h-96">
+              <img
+                src={`${imageURL}${item.poster_path}`}
+                alt={item.title}
+                className="absolute w-full h-full object-cover"
+              />
+            </div>
+
+            {/* Text container */}
+            <div className="bg-[#4c545b] w-full h-44 gap-4 p-4">
+              <div className="flex justify-between text-white">
+                <h2>{item.title}</h2>
+              </div>
+              <p className="mt-4 text-white">{item.overview.slice(0, 40)}...</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </>
+  ) : (
+    <>
+      {movieData?.results
+        .filter((item) => item.release_date <= currentDate)
+        .map((item) => (
+          <div
+            key={item.id}
+            className="lg:hover:scale-110 md:hover:scale-110 md:hover:border md:hover:shadow-2xl md:hover:shadow-gray-600 lg:hover:border lg:hover:shadow-2xl lg:hover:shadow-gray-600 transition msm:w-[85%] msm:h-[50%] msm:mb-52 duration-500 ease-in-out"
+          >
+            {/* Image container */}
+            <Link href={`/Movies/${item.id}`}>
+              {/* p-10 */}
+              <div className="msm:w-full msm:h-full md:w-full md:h-64 lg:w-full lg:h-96 relative">
+                <img
+                  src={`${imageURL}${item.poster_path}`}
+                  alt={item.title}
+                  // layout="fill"
+                  // objectFit="cover"
+                  className="absolute w-full h-full object-cover"
+                  // priority
+                />
+              </div>
+            </Link>
+
+            {/* Text container */}
+            <div className="bg-[#4c545b] flex flex-col h-44 cards msm:h-full card-text-container">
+              <Link
+                href={`/Movies/${item.id}`}
+                className="flex lg:ml-4 h-10 text-white justify-between"
+              >
+                <div className="msm:w-[55%]">
+                  <h2>{item.title}</h2>
+                </div>
+                <div className="flex gap-2">
+                  <span className={`${getVotecolor(item.vote_average)}`}>
+                    {item.vote_average.toString().slice(0, 3)}
+                  </span>
+                  <FaStar color="yellow" style={{ marginTop: "3px" }} />
+                </div>
+              </Link>
+              {/* watchlist and review container */}
+              <div className="movies-buttons-container flex flex-col justify-center gap-4">
+                <div className="flex justify-center mt-4 ml-[-2rem]">
+                  <AddToWatchlist movieId={item.id} />
+                </div>
+                <span className="text-white justify-center text-center">
+                  <Link href={`/Movies/${item.id}/reviews`}>Review</Link>
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default Cards;

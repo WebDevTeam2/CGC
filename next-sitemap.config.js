31739da454ecec1e275cb4e419287e4eac3d3332
Stefanos2002 <kalouliss@gmail.com>
2024-10-26 20:09:34 +0300

    update, looking to fix double link result

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.cinegame-critic.com", // Replace with your site's URL
-  // generateRobotsTxt: true, // Optional: generate a robots.txt
+  siteUrl: "https://www.cinegame-critic.com", // Set your site URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+  exclude: [
+    "/Movies/TVShows/*", // Exclude dynamic subpages under /TVShows, if not necessary for SEO
+    "/Signin", // Exclude login, signup, and other utility pages
+    "/Signup",
+    "/Verified",
+  ],
+  changefreq: "daily", // Set default change frequency
+  priority: 0.7, // Set default priority for pages
+  sitemapSize: 5000, // Max number of URLs per sitemap file
+  additionalPaths: async (config) => [
+    { loc: "/Movies/Movies-trending", changefreq: "daily", priority: 0.9 },
+  ],
 };

f7c114b551917958061b492ba064c4500c178e78
UnLakhs <apostolos052@gmail.com>
2024-11-02 18:45:17 +0200

    better styles on movie user reviews

--- app/components/Movie-components/UserMovieReviews.tsx
+++ app/components/Movie-components/UserMovieReviews.tsx
@@ -22,13 +22,13 @@ const UserMovieReviews = async ({ movieId }: { movieId: Number }) => {
   return (
     <>
       {reviewsForMovie && reviewsForMovie.length > 0 ? (
-        <div className="border border-gray-300 shadow-gray-600 p-4 rounded-md shadow-md w-80 h-64 md:w-48 md:h-96 lg:h-96 lg:w-72 overflow-auto">
+        <div className="border border-gray-300 shadow-gray-600 text-slate-900 bg-[#5d676f] p-4 rounded-lg shadow-md w-80 h-64 md:w-60 md:h-96 lg:h-96 lg:w-72 overflow-auto user-review-container">
           {reviewsForMovie.map((review: any) => {
             return (
-              <div key={review.reviewId} className="my-2 p-2">
-                <div className="flex gap-2 md:gap-6">
-                  <span className="font-semibold w-1/2">{review.date}</span>
-                  <span className="font-semibold w-1/2">{review.username}</span>
+              <div key={review.reviewId} className="my-2 p-4 rounded-lg shadow-md border border-gray-400 bg-slate-300 user-review-text-container">
+                <div className="flex justify-between gap-2 md:gap-6 mb-1">
+                <span className="text-xs font-semibold user-review-date">{new Date(review.date).toLocaleString()}</span> {/*The date is converted based on the user's location */}
+                  <span className="text-xs font-semibold max-w-[4rem] overflow-hidden text-ellipsis whitespace-nowrap">{review.username}</span>
                 </div>
 
                 <div className="w-full mt-1 overflow-hidden text-ellipsis whitespace-normal break-words">
@@ -39,8 +39,8 @@ const UserMovieReviews = async ({ movieId }: { movieId: Number }) => {
           })}
         </div>
       ) : (
-        <div className="my-4 mb-10 flex w-full justify-center items-center">
-          <span className="bg-neutral-600 sm:text-lg text-md text-slate-200 z-20 py-3 px-6 rounded-lg">
+        <div className="border border-gray-300 shadow-gray-600 text-slate-100 bg-[#5d676f] p-4 rounded-lg shadow-md w-80 md:w-60 lg:w-72 overflow-auto user-review-container">
+          <span className="">
             No reviews at this moment.
           </span>
         </div>

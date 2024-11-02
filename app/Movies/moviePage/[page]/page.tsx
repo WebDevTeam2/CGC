0a8a355335e255e900937c94d6936e2165e25af8
Stefanos2002 <kalouliss@gmail.com>
2024-11-02 21:27:22 +0200

    .

--- app/Movies/moviePage/[page]/page.tsx
+++ app/Movies/moviePage/[page]/page.tsx
@@ -1,6 +1,6 @@
 import Filter from "@/app/components/Movie-components/Filter";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "@/authDbConnection/authOptions";
 import { findUserByEmail } from "@/app/User Collection/connection";
 import { baseUrl, Movie, options } from "@/app/constants/constants";
 import Cards from "@/app/components/Movie-components/Cards";

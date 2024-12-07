55fbf5f4ecdda68099c933e35709de4ca41ea4fc
Stefanos2002 <kalouliss@gmail.com>
2024-12-07 17:08:41 +0200

    .

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,12 @@
 import { createRouteHandler } from "uploadthing/next";
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
 
 // import { ourFileRouter } from "./core";
 import { ourFileRouter } from "./core"; // Path to your file router
+import type { OurFileRouter } from "./core";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
@@ -10,3 +15,6 @@ export const { GET, POST } = createRouteHandler({
   // Apply an (optional) custom config:
   // config: { ... },
 });
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();

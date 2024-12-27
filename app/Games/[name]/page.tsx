59413e29e1c26e7407f12efd9eb14bc2fd97b1e7
Stefanos2002 <kalouliss@gmail.com>
2024-12-27 17:46:39 +0200

    updates

--- app/Games/[name]/page.tsx
+++ app/Games/[name]/page.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link";
 import Footer from "@/app/Components/Footer";
 import GameDets from "@/app/Components/Game-components/GameDets";
 import UserReviews from "@/app/Components/Game-components/UserReviews";
 import NavBar from "@/app/Components/Game-components/NavBar";
-import SearchBar from "@/app/Components/Game-components/SearchBar";
 import { fetchAndCombineDataSimple } from "@/app/Game Collection/functions";
 
 export default async function Games({ params }: { params: any }) {
@@ -11,7 +9,7 @@ export default async function Games({ params }: { params: any }) {
   return (
     <div>
       <NavBar />
-      <div className="bg-[url('https://img.freepik.com/free-photo/3d-rendering-hexagonal-texture-background_23-2150796421.jpg?t=st=1734181088~exp=1734184688~hmac=67b5c36d583dffaeca547f04e9c70455b5d4c4813ced5a85e9239922e34757dc&w=1380')] z-0 bg-cover fixed h-screen w-screen"></div>
+      <div className="bg-[url('/assets/images/back.jpg')] z-0 bg-cover fixed h-screen w-screen"></div>
       <GameDets params={params} />
       <UserReviews params={params} />
       <Footer />

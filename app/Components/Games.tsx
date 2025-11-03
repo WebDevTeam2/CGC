58297c6dbac7a66bf1cfa6db4dc740cf92c7745a
Stefanos2002 <kalouliss@gmail.com>
2025-11-03 13:01:22 +0200

    tested Image option

--- app/Components/Games.tsx
+++ app/Components/Games.tsx
@@ -10,10 +10,11 @@ function Games() {
       href={`/Games/page/1`}
       className="relative group text-6xl hover:scale-105 justify-center flex lg:w-2/4 w-full h-screen overflow-hidden transition duration-500 ease-in-out cursor-pointer"
     >
-      <img
+      <Image
         src={senuaImagePath}
         alt="Senua image placeholder"
         className="absolute object-cover h-full w-full grayscale hover:grayscale-0 transition duration-1000"
+        priority
       />
       <h1 className="group-hover:opacity-100 transition duration-1000 flex absolute top-2/4 text-white z-10 lg:text-[4rem] text-[2rem] lg:opacity-0">
         Games
--- app/Components/Movies.tsx
+++ app/Components/Movies.tsx
@@ -57,10 +57,11 @@ const Movies = () => {
       // onMouseEnter={handleMouseEnter}
       // onMouseLeave={handleMouseLeave}
     >
-      <img
+      <Image
         src={jokerImagePath}
         alt="Joker image placeholder"
         className="absolute object-cover h-full w-full grayscale hover:grayscale-0 transition duration-1000  "
+        priority
       />
       <h1 className="clip-text group-hover:opacity-100 transition duration-1000 flex absolute top-2/4 text-white z-10 lg:text-[4rem] text-[2rem] lg:opacity-0">
         Movies
